1ab000d1185dc6e6f290aeabbeee7fe9ac99a5ea
pdusenge <p.dusenge@alustudent.com>
2025-07-02 08:21:20 +0000

    new alternative

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -14,4 +14,4 @@ def top_ten(subreddit):
         HOT_POSTS = RESPONSE.json().get("data").get("children")
         [print(post.get('data').get('title')) for post in HOT_POSTS]
     except Exception:
-        print('OK')
+        print(None)
