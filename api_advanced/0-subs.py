0a0c5eedb6d0b0bbb8fcd864bbd008853a2b990c
pdusenge <p.dusenge@alustudent.com>
2025-07-02 07:39:58 +0000

    removing unecessary changes

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -2,16 +2,13 @@
 """DOC"""
 import requests
 
-
 def number_of_subscribers(subreddit):
     """DOC"""
     reddit_url = "https://www.reddit.com/r/{}/about.json" \
         .format(subreddit)
 
     header = {'User-agent': 'Mozilla/5.0'}
-    response = requests.get(reddit_url,
-                            headers=header
-                            )
+    response = requests.get(reddit_url, headers=header)
 
     if response.status_code == 200:
         data = response.json()['data']
